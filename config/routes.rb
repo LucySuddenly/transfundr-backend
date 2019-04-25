270215617926cc797fd28d63804a642f456073f1
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-25 10:26:21 -0700

    added login auth controller and action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :donations
   resources :beacons
   resources :users
+  post '/auth', to: "auth#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
