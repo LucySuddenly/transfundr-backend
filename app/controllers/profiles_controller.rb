0d8491c1186c52adea8089e1765434006eea5af0
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-25 14:04:20 -0700

    added get profile by user id action and route

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,15 @@
 class ProfilesController < ApplicationController
-    skip_before_action :authorized, only: [:show]
+    skip_before_action :authorized, only: [:get_profile_by_user_id]
 
     def create
         profile = Profile.new(profile_params)
         profile.user_id = token_user_id
         profile.save
+        render json: profile
+    end
+
+    def get_profile_by_user_id
+        profile = Profile.find_by(user_id: params[:id])
         render json: profile
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :beacons
   resources :users
   post '/auth', to: "auth#create"
+  get '/getprofilebyuserid/:id',  to: "profile#get_profile_by_user_id"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
