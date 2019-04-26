ca417780bec02d0ffff21b30037a0edffd6b410d
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-25 18:34:23 -0700

    added new beacon form, connected it to the backend

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,2 +1,13 @@
 class BeaconsController < ApplicationController
+
+    def create
+        beacon = Beacon.new(beacon_params)
+        beacon.user_id = token_user_id
+        beacon.save
+        render json: beacon
+    end
+
+    def beacon_params
+        params.permit(:title, :text, :target)
+    end
 end
