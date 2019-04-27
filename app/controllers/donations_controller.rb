ae3db8b57331a6fca322d2ac991115f6d5815f6d
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-26 17:18:23 -0700

    updated donation show render to include nested json data

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,19 @@
 class DonationsController < ApplicationController
+    skip_before_action :authorized, only: [:show]
+
+    def create 
+        donation = Donation.create(donation_params)
+        render json: donation
+    end
+
+    def show
+        donation = Donation.find(params[:id])
+        render json: donation.to_json(:include => [{ :user => {:include => :profile}}, {:beacon => {:include => {:user => {:include => :profile}}}}])
+    end
+
+    private
+
+    def donation_params
+        params.permit(:user_id, :beacon_id, :amount, :points, :confirmed)
+    end
 end
