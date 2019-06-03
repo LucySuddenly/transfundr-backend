c934c46536402ac36e0ae3814256b190fa556489
Lucy Suddenly <lucysuddenly@gmail.com>
2019-06-03 15:43:59 -0700

    added mailer view

--- app/mailers/mailer_mailer.rb
+++ app/mailers/mailer_mailer.rb
@@ -1,7 +1,7 @@
 class MailerMailer < ApplicationMailer
     default from: "transfundr@gmail.com"
 
-  def sample_email(user)
+  def donation_email(user)
     @user = user
     mail(to: @user.email, subject: 'You have a new donation!')
   end
